81575df82e57be8633e0fecca1f14dba3810bd17
SusuYes <SusuYes@users.noreply.github.com>
2026-01-21 23:19:40 +1100

    Updates

--- _next/static/n9mSdDaZd9BFptWO4dvx5/_buildManifest.js
+++ _next/static/n9mSdDaZd9BFptWO4dvx5/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/abuosba-landing-v2//_next/:path+",
+        "destination": "/abuosba-landing-v2/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
